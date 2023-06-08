542e93d4c1e1767868074783faae78d75e7d2806
cyeji <choyeji1591@gmail.com>
2023-06-09 07:44:29 +0900

    first commit

---
+++ src/test/java/com/ktw/section8/Main1Test.java
@@ -0,0 +1,46 @@
+package com.ktw.section8;
+
+import static com.ktw.section8.Main1.solution;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.ktw.common.TestFileUtil;
+import java.io.BufferedReader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("타이타닉 침몰")
+class Main1Test {
+
+    @Test
+    @DisplayName("타이타닉 침몰 케이스1")
+    void test_case_1 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section8/main1/test_case1.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(3 , solution);
+    }
+
+    @Test
+    @DisplayName("타이타닉 침몰 케이스2")
+    void test_case_2 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section8/main1/test_case2.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(5 , solution);
+    }
+
+    @Test
+    @DisplayName("타이타닉 침몰 케이스3")
+    void test_case_3 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section8/main1/test_case3.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(14 , solution);
+    }
+}

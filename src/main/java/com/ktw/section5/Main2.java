70785479b7adac32d7876ae4dca37a2ec2c257ed
cyeji <choyeji1591@gmail.com>
2023-06-19 12:44:11 +0900

    fix : 패키지 이동

--- src/main/java/com/ktw/section5/Main2.java
+++ src/main/java/com/ktw/section5/Main2.java
@@ -1,4 +1,4 @@
-package com.ktw.section8;
+package com.ktw.section5;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,6 @@ import java.util.Arrays;
 
 /**
  * 이동횟수
- *
  * 현수는 n개의 물건 A -> B
  * 한번에 5kg 이하만 들고 이동할 수 있습니다.
  * 최소이동횟수
@@ -22,19 +21,19 @@ public class Main2 {
         }
     }
 
-    static int solution(BufferedReader br) throws IOException{
+    static int solution(BufferedReader br) throws IOException {
         int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         Arrays.sort(nums);
         int answer = 0;
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
 
-        while (left<=right){
-            if(nums[left]+nums[right] <= 5){
+        while (left <= right) {
+            if (nums[left] + nums[right] <= 5) {
                 answer++;
                 left++;
                 right--;
-            }else{
+            } else {
                 right--;
                 answer++;
             }

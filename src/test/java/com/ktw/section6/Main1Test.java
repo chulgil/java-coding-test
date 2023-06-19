faa1e99e95cf0e4be46977b7a9bab894bbf898e0
cyeji <choyeji1591@gmail.com>
2023-06-19 11:33:24 +0900

    test : 가장 가까운 큰수 테스트 추가

---
+++ src/test/java/com/ktw/section6/Main1Test.java
@@ -0,0 +1,69 @@
+package com.ktw.section6;
+
+import static com.ktw.section6.Main1.solution;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.ktw.common.TestFileUtil;
+import java.io.BufferedReader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("가장 가까운 큰수")
+class Main1Test {
+
+
+    @Test
+    @DisplayName("가장 가까운 큰수 케이스1")
+    void test_case_1() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section6/main1/test_case1.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(132, solution);
+    }
+
+    @Test
+    @DisplayName("가장 가까운 큰수 케이스2")
+    void test_case_2() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section6/main1/test_case2.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(-1, solution);
+    }
+
+    @Test
+    @DisplayName("가장 가까운 큰수 케이스3")
+    void test_case_3() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section6/main1/test_case3.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(20735, solution);
+    }
+
+    @Test
+    @DisplayName("가장 가까운 큰수 케이스4")
+    void test_case_4() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section6/main1/test_case4.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(71127, solution);
+    }
+
+    @Test
+    @DisplayName("가장 가까운 큰수 케이스5")
+    void test_case_5() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/section6/main1/test_case5.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(54321, solution);
+    }
+}
